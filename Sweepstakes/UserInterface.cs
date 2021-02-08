22ef4b217e27a198c05024a5cacd2c485df08bac
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-08 11:46:17 -0500

    Implemented one functionality for the UserInterface class.

---
+++ Sweepstakes/UserInterface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    static class UserInterface
+    {
+        public static string GetUserInputFor(string prompt)
+        {
+            switch (prompt)
+            {
+                case "Assign Contestant":
+                    Contestant contestant = new Contestant();
+                    Console.WriteLine("Please enter the first name of the contestant:");
+                    contestant.FirstName = Console.ReadLine();
+                    Console.WriteLine("Please enter the last name of the contestant:");
+                    contestant.LastName = Console.ReadLine();
+                    Console.WriteLine("Please enter the email address of the contestant:");
+                    contestant.EmailAddress = Console.ReadLine();
+                    Console.WriteLine("Please enter the registration number of the contestant:");
+                    contestant.RegistrationNumber = Int32.Parse(Console.ReadLine());
+                    break;
+            }
+            return "Done";
+        }
+    }
+}
