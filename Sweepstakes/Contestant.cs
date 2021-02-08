955f6cea113a6437beed09f17450d8691b021dfe
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-08 10:47:12 -0500

    Implemented Contestant class.

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        public int RegistrationNumber
+        {
+            get;
+            set;
+        }
+    }
+}
