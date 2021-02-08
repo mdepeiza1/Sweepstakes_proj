81dbd3b928629e31361d030da3281fb658c887a6
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-08 13:25:00 -0500

    MarketingFirm class was implemented.

---
+++ Sweepstakes/MarketingFirm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class MarketingFirm
+    {
+        private ISweepstakesManager _manager;
+
+        public MarketingFirm(ISweepstakesManager manager)
+        {
+            _manager = manager;
+        }
+
+        public void CreateSweepstake()
+        {
+            _manager.InsertSweepstakes(new Sweepstakes(UserInterface.GetUserInputFor("What is the name of the sweepstakes?")));
+        }
+    }
+}
