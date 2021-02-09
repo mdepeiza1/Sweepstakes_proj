716a84f872056730151b50653dfdb9812d0793d1
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-09 13:01:12 -0500

    Updated Simulation class with a comment.

--- Sweepstakes/Simulation.cs
+++ Sweepstakes/Simulation.cs
@@ -10,7 +10,7 @@ namespace Sweepstakes
     {
         // REFACTOR: Create a separate ISweepStakesManagerFactory class - static class
         // Refactor the method to return type ISweepStakesManager + use a switch case
-        public MarketingFirm marketingFirm;
+        public MarketingFirm marketingFirm; //might have to change this
         public void CreateMarketingFirmWithManager() // this method returns type ISweepStakesManager, may need to add "string type" to parameters 
         {
             marketingFirm = new MarketingFirm(ISweepstakesManagerFactory.SweepstakesManagerFactory(UserInterface.GetUserInputFor("Should the manager be queue-based or stack-based? Please type stack or queue."))); // this is where the factory method to pick sweepstakes managers comes in
